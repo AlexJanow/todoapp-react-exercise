e99120f4d1589b8dbb67abc48aad6844b4a8d649
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-15 11:44:28 +0200

    add simple list adding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  function renderTodoItems() {
+    const liItems = todoItems.map((todoItem) => {
+      return (
+        <TodoItem key={todoItem} name={todoItem} onClick={handleClickTodo} />
+      );
+    });
+    return liItems;
+  }
+
+  function handleClickTodo(clickedTodo) {
+    const newTodos = todoItems.filter((todoItem) => todoItem !== clickedTodo);
+    setTodoItems(newTodos);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const newTodoItem = form.todoItemName.value;
+
+    const newTodoItems = [...todoItems, newTodoItem];
+    setTodoItems(newTodoItems);
+    form.reset();
+  }
+
+  function TodoItem({ name, onClick }) {
+    function handleClick() {
+      onClick(name);
+    }
+    return <li onClick={handleClick}>{name}</li>;
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add todo"
+          name="todoItemName"
+          id="todoItemName"
+          required
+        />
+        <button type="submit"> Add</button>
+      </form>
+      <ul>{renderTodoItems()}</ul>
     </div>
   );
 }
